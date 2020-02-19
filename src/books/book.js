d71d8d81af0332238c4ecc45d3881e5255ef2f82
carsonfidler <fidler.45@osu.edu>
2020-02-19 18:27:54 -0500

    added more book cards

--- src/books/book.js
+++ src/books/book.js
@@ -12,13 +12,37 @@ export default class Books extends Component {
                     id: 1,
                     title: "Domain Driven Design",
                     author: "Eric Evans",
-                    isbn: "978-1942788331"
+                    isbn: "978-0321125217"
                 },
                 {
                     id: 2,
                     title: "Accelerate",
                     author: "Nicole Forsgren",
                     isbn: "978-1942788331"
+                },
+                {
+                    id: 3,
+                    title: "Agile Framework",
+                    author: "Jim Smith",
+                    isbn: "978-9304802454"
+                },
+                {
+                    id: 4,
+                    title: "RFP Project Management",
+                    author: "Harper Roberts",
+                    isbn: "978-9404924854"
+                },
+                {
+                    id: 5,
+                    title: "Discrete Mathematics 5th Edition",
+                    author: "Sam Fisher",
+                    isbn: "978-1324598534"
+                },
+                {
+                    id: 6,
+                    title: "Physics: Thermodynamics, Mechanical, and Waves 6th Edition",
+                    author: "Will Chui",
+                    isbn: "978-3455394083"
                 }
             ]
         };
