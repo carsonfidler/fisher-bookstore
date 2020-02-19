86c517544e2ac2b4538a78288f4436dd0320834e
carsonfidler <fidler.45@osu.edu>
2020-02-18 19:39:06 -0500

    please sync

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import "./App.css";
 import { Navbar } from "react-bootstrap";
+import { NavItem } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Routes from "./Routes";
 
@@ -10,10 +12,18 @@ export default class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="lg" expand="lg">
           <Navbar.Brand>
             <Link to="/">Fisher Bookstore</Link>
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <NavItem>
+                <Nav.Link href="/books">Books</Nav.Link>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./homes/Home";
+import Books from "./books/book";
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/books" component={Books} />
         </Switch>
-    )
+    );
 }
