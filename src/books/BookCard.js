6b57e894ef731157eaa6559114d8e28d5a205ff0
carsonfidler <fidler.45@osu.edu>
2020-02-19 17:44:38 -0500

    updated link

--- src/books/BookCard.js
+++ src/books/BookCard.js
@@ -4,7 +4,7 @@ import { Card, Button } from "react-bootstrap";
 export function BookCard(props) {
     return(
         <Card style={{ width: "16em" }}>
-            <Card.Img variant="top" src=https://via.placeholder.com/150" />
+            <Card.Img variant="top" src="https://via.placeholder.com/150" />
             <Card.Body>
                 <Card.Title>{props.book.title}</Card.Title>
                 <Card.Text>Author: {props.book.author}</Card.Text>
