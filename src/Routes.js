bd020fee81bf9f5b1b08e95960095a4ee74bc28c
carsonfidler <fidler.45@osu.edu>
2020-02-18 19:54:28 -0500

    pls work

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./homes/Home";
 import Books from "./books/book";
+import Authors from "./authors/author";
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/books" component={Books} />
+            <Route path="/authors" component={Authors} />
         </Switch>
-    );
+    )
 }
---
+++ src/authors/author.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./author.css";
+
+export default function Author() {
+    return (
+        <div className="Author">
+            <div className="lander">
+                <h1>Author</h1>
+                <p> Here are the authors</p>
+            </div>
+        </div>
+    );
+}
