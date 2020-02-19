82cc5d9a44647c88727f9d9a41b31279bfe70e79
carsonfidler <59670819+carsonfidler@users.noreply.github.com>
2020-02-19 18:59:03 -0500

    Array authors feature (#23)
    * author cards draft
    
    * author cards initial

--- src/authors/author.js
+++ src/authors/author.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import "./author.css";
+import { AuthorDisplay } from "./AuthorDisplay";
 
-export default function Author() {
-    return (
-        <div className="Author">
-            <div className="lander">
-                <h1>Author</h1>
-                <p> Here are the authors</p>
+
+export default class Authors extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authors: [
+                {
+                    id: 1,
+                    author: "Eric Evans",
+                    author_id: "193573"
+                },
+                {
+                    id: 2,
+                    author: "Nicole Forsgren",
+                    author_id: "193253"
+                },
+                {
+                    id: 3,
+                    author: "Jim Smith",
+                    author_id: "193463"
+                },
+                {
+                    id: 4,
+                    author: "Harper Roberts",
+                    author_id: "194905"
+                },
+                {
+                    id: 5,
+                    author: "Sam Fisher",
+                    author_id: "194954"
+                },
+                {
+                    id: 6,
+                    author: "Will Chui",
+                    author_id: "192309"
+                }
+            ]
+        };
+    }
+    render() {
+        return (
+            <div className="Authors">
+                <div className="lander">
+                    <AuthorDisplay authors={this.state.authors} />
+                </div>
             </div>
-        </div>
-    );
+        );
+}
 }
